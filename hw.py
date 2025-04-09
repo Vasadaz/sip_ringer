c3e2f18dd5f3f92a90197a791f5ac3379b896b0f
Nikolai Sysoev <vasadaz@ya.ru>
2025-04-09 12:10:02 +0300

    Add Call - BAD

--- hw.py
+++ hw.py
@@ -1,27 +1,39 @@
-import pjsua2 as pj
 import time
-
+import pjsua2 as pj
 from environs import Env
+from pjsua2 import Endpoint
 
 
-class Account(pj.Account):
+class MyCall(pj.Call):
+    def __init__(self, acc: pj.Account, call_id: int = -1):
+        super().__init__(acc, call_id)
+
+    def onCallMediaState(self, prm):
+        ci = self.getInfo()
+        for media in ci.media:
+            if media.type == pj.PJMEDIA_TYPE_AUDIO:
+                aud_med = pj.AudioMedia.typecastFromMedia(self.getMedia(media.index))
+                ep.audDevManager().getCaptureDevMedia().startTransmit(aud_med)
+                aud_med.startTransmit(ep.audDevManager().getPlaybackDevMedia())
+                print("Аудио подключено.")
+
+
+class MyAccount(pj.Account):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+
     def onRegState(self, prm):
-        print('***OnRegState: ' + prm.reason)
+        print(f"Статус регистрации: код {prm.code}, причина: {prm.reason}")
 
 
-def pjsua2_test(
-    username: str,
-    password: str,
-    domain: str,
-    port: int,
-):
-    ep_cfg = pj.EpConfig()
-    ep_cfg.uaConfig.threadCount = 0
-    ep_cfg.logConfig.level = 5
-    ep_cfg.uaConfig.maxCalls = 4
+def main(username: str, password: str, domain: str, port: int, dest_uri: str):
+    global ep
 
     ep = pj.Endpoint()
     ep.libCreate()
+
+    ep_cfg = pj.EpConfig()
     ep.libInit(ep_cfg)
 
     t_cfg = pj.TransportConfig()
@@ -30,22 +42,42 @@ def pjsua2_test(
 
     ep.libStart()
 
+    ep.audDevManager().setNullDev()
+    print("Конечная точка pjsua2 запущена без использования реального аудиоустройства.")
+
     acc_cfg = pj.AccountConfig()
-    acc_cfg.idUri = f'I-{username} <sip:{username}@{domain}>'
+    acc_cfg.idUri = f'sip:{username}@{domain}'
+
+    acc_cfg.sipConfig.contactUri = f"sip:{username}@{domain}"
     acc_cfg.regConfig.registrarUri = f'sip:{domain}'
-    cred = pj.AuthCredInfo('digest', '*', username, 0, password)
+    cred = pj.AuthCredInfo("digest", "*", username, 0, password)
     acc_cfg.sipConfig.authCreds.append(cred)
 
-    acc = Account()
+    acc = MyAccount(acc_cfg)
     acc.create(acc_cfg)
+    time.sleep(3)
 
-    time.sleep(100)
+    call = MyCall(acc)
+    call_param = pj.CallOpParam(True)
+    full_dest_uri = f'sip:{dest_uri}'
+    print(f"Выполняется вызов на {full_dest_uri}...")
+
+    try:
+        call.makeCall(full_dest_uri, call_param)
+    except pj.Error as e:
+        print(f"Ошибка при вызове: {e}")
+        return
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Завершение программы...")
 
     ep.libDestroy()
-    del ep, acc
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     env = Env()
     env.read_env()
     sip_domain = env.str('SIP_DOMAIN')
@@ -53,11 +85,11 @@ if __name__ == '__main__':
     sip_username = env.str('SIP_USERNAME')
     sip_password = env.str('SIP_PASSWORD')
     called_number = env.str('CALL_NUMBER')
-
-    pjsua2_test(
+    main(
         username=sip_username,
         password=sip_password,
         domain=sip_domain,
         port=sip_port,
+        dest_uri=called_number
     )
 
